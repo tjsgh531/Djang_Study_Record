92351ed7ab61936900942b916017e32edba5df55
Sun Ho <tjsgh5312@daum.net>
2021-10-14 17:05:07 +0900

    GET POST

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
+
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'post' : "이게 작동 된다"})
+
     return render(request, 'accountapp/hello_world.html', context={})
