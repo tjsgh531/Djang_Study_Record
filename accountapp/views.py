28bc458e51f6ee83d1047ae02584b44e037206c9
Sun Ho <tjsgh5312@daum.net>
2021-10-22 13:15:17 +0900

    django curse 18 commit - HttpResponseRedirect(reverse()) - {% for %}{% endfor %}

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from .models import HelloWorld
 
 
@@ -11,9 +13,7 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
