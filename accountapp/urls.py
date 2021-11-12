dc1339740228bc6333c900a51d3283916c1d52e0
Sun Ho <tjsgh5312@daum.net>
2021-11-12 18:15:34 +0900

    django course 24 commits - detailview * <int:pk> * {{ user.date_joined }} * {{ user.username }}

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from .views import AccountCreateView, hello_world
+from .views import AccountCreateView, hello_world, AccountDetailView
 
 app_name = "accountapp"
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
    path('logout/', LogoutView.as_view(), name='logout'),
 
-   path('create/', AccountCreateView.as_view(), name='create')
+   path('create/', AccountCreateView.as_view(), name='create'),
+   path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from django.contrib.auth.models import User
 from .models import HelloWorld
@@ -20,8 +20,12 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
-class AccountCreateView(CreateView):
+class AccountCreateView(CreateView): # 회원가입
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView): # 마이페이지
+    model = User
+    template_name = 'accountapp/detail.html'
